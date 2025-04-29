c40383be59edebdfb430f39cdde203ccf1c9418b
TLZ2000 <thomas@cnt3.com>
2025-04-29 11:37:30 +0200

    WIP BFS

--- prova/grafoMatrix.js
+++ prova/grafoMatrix.js
@@ -5,7 +5,8 @@ const client = new DeliverooApi(
     // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjJjOTQyMSIsIm5hbWUiOiJtYXJjbyIsInRlYW1JZCI6IjViMTVkMSIsInRlYW1OYW1lIjoiZGlzaSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQyNTY3NDE4fQ.5m8St0OZo_DCXCriYkLtsguOm1e20-IAN2JNgXL7iUQ'
     //'https://deliveroojs2.rtibdi.disi.unitn.it/',
     // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQyNmQ1NyIsIm5hbWUiOiJtYXJjbyIsInRlYW1JZCI6ImM3ZjgwMCIsInRlYW1OYW1lIjoiZGlzaSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQwMDA3NjIwfQ.1lfKRxSSwj3_a4fWnAV44U1koLrphwLkZ9yZnYQDoSw'
-    'http://localhost:8080'
+    'http://localhost:8080', 
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImI5OGY4NyIsIm5hbWUiOiJBZ2VudCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQ1OTEzMjgzfQ.fO8zWZQNHmMP1ULrkH11xE6u9aUHGSin1OO-_fgqaiY"
 )
 
 function distance( {x:x1, y:y1}, {x:x2, y:y2}) {
@@ -61,9 +62,8 @@ class GraphNode{
         this.neighD = undefined;
         this.neighL = undefined;
         this.type = type;
-        this.nearestDelivery = undefined;
-        this.nearestSpawn = undefined;
-        console.log("creato [" + x + ", " + y + "] type " + type)
+        this.nearestDelivery = [];      // List of nearest delivery areas
+        this.nearestSpawn = undefined;  // Nearest spawn area
     }
 }
 
@@ -76,22 +76,20 @@ class Graph{
         for(let x=0; x < this.gameMap.width; x++){
             this.graphMap[x]=[];
             for(let y=0; y < this.gameMap.height; y++){
-                if(this.gameMap.map[x][y].type!=0){
-                    this.graphMap[x][y] = new GraphNode(this.gameMap.map[x][y].type, x, y);
-                } else {
+                if(this.gameMap.getItem(x, y)==0){
                     this.graphMap[x][y] = undefined;
-                }
+                } else {
+                    this.graphMap[x][y] = new GraphNode(this.gameMap.getItem(x, y), x, y);
+                }                
             }
         }
 
         // Connect each graph node to its neighbors
-        this.graphMap.forEach((x)=>{
-            x.forEach((y) =>{
-                // if undefined this is unwalkable
+        for(let x=0; x < this.gameMap.width; x++){
+            for(let y=0; y < this.gameMap.height; y++){
                 if(this.graphMap[x][y]!=undefined){
-
                     // if neighbor in bound
-                    if(0<=y+1 && y+1 < height){
+                    if(0<=y+1 && y+1 < this.gameMap.height){
                         // if neighbor walkable
                         if(this.graphMap[x][y+1]!=undefined){
                             // add up neighbor
@@ -99,16 +97,83 @@ class Graph{
                         }
                     }
 
-                    //TODO: controlla altri vicini
-                    let right = [x+1, y];
-                    let down = [x, y-1];
-                    let left = [x-1, y];
+                    // if neighbor in bound
+                    if(0<=x+1 && x+1 < this.gameMap.width){
+                        // if neighbor walkable
+                        if(this.graphMap[x+1][y]!=undefined){
+                            // add right neighbor
+                            this.graphMap[x][y].neighR = this.graphMap[x+1][y];
+                        }
+                    }
+
+                    // if neighbor in bound
+                    if(0<=y-1 && y-1 < this.gameMap.height){
+                        // if neighbor walkable
+                        if(this.graphMap[x][y-1]!=undefined){
+                            // add down neighbor
+                            this.graphMap[x][y].neighD = this.graphMap[x][y-1];
+                        }
+                    }
+
+                    // if neighbor in bound
+                    if(0<=x-1 && x-1 < this.gameMap.width){
+                        // if neighbor walkable
+                        if(this.graphMap[x-1][y]!=undefined){
+                            // add left neighbor
+                            this.graphMap[x][y].neighL = this.graphMap[x-1][y];
+                        }
+                    }
                 }
-            })
-        })
+            }
+        }
+
+        /*
+        for (let i = this.gameMap.width - 1; i >= 0; i--) {
+            for (let j = 0; j < this.gameMap.height; j++) {
+                if(this.graphMap[i][j] == undefined){
+                    console.log("[" + i + "][" + j + "]=0");
+                } else {
+                    console.log("[" + i + "][" + j + "]");
+                        if(this.graphMap[i][j].neighU == undefined){
+                            console.log("U=0")
+                        } else {
+                            console.log("U=[" + this.graphMap[i][j].neighU.x + "][" + this.graphMap[i][j].neighU.y + "]");
+                        }
+                        if(this.graphMap[i][j].neighR == undefined){
+                            console.log("R=0")
+                        } else {
+                            console.log("R=[" + this.graphMap[i][j].neighR.x + "][" + this.graphMap[i][j].neighR.y + "]");
+                        }
+                        if(this.graphMap[i][j].neighD == undefined){
+                            console.log("D=0")
+                        } else {
+                            console.log("D=[" + this.graphMap[i][j].neighD.x + "][" + this.graphMap[i][j].neighD.y + "]");
+                        }
+                        if(this.graphMap[i][j].neighL == undefined){
+                            console.log("L=0")
+                        } else {
+                            console.log("L=[" + this.graphMap[i][j].neighL.x + "][" + this.graphMap[i][j].neighL.y + "]");
+                        }
+                }
+                console.log("\n----------------------\n")                
+            }
+        }*/
+       console.log("DONE")
+    }
+
+    // Compute nearest delivery/spawn
+    preprocess(){
+        // Compute nearest delivery
+        let queue = new Queue();
+        this.gameMap.deliveryZones.array.forEach(element => {
+            // Check all delivery zones
+            queue.enqueue(element);
+
+            // Set local delivery zones
+            this.graphMap[element.x][element.y].nearestDelivery.push({x:element.x, y:element.y});
+        });
     }
 }
-
 
 class GameMap{
     constructor(width, height, tile){
@@ -116,50 +181,29 @@ class GameMap{
         this.width = width;
         this.height = height;
         this.map = [];
-        this.delivery_cells = []; // {x, y, type}
-        this.spawn_cells = [];
-        this.walkable_cells = [];
-        this.closest_delivery_cell = [];
-        this.closest_spawn_cell = [];
+        this.deliveryZones = [];
+        this.spawnZones = [];
 
         for (let i = 0; i < width; i++) {
             this.map[i] = [];
-            for (let j = 0; j < height; j++) {
-                this.map[i].push(tile[i + width*j])
-                if(this.map[i][j].type == 2) {
-                    this.delivery_cells.push(this.map[i][j])
-                }
-                if(this.map[i][j].type == 1) {
-                    this.spawn_cells.push(this.map[i][j])
-                }
-                if(this.map[i][j].type != 0) {
-                    this.walkable_cells.push(this.map[i][j])
-                }
-            }
         }
-    
-        for (let i = 0; i < width; i++) {
-            this.closest_delivery_cell[i] = [];
-            this.closest_spawn_cell[i] = [];
-            for (let j = 0; j < height; j++) {
-                
+
+        for(let i = 0; i < (width*height); i++){
+            let currentItem = tile[i];
+            this.map[currentItem.x][currentItem.y]=currentItem.type;
+
+            if(currentItem.type == 1){
+                // Spawn zone
+                this.spawnZones.push(currentItem);
+            } else if (currentItem.type == 2){
+                // Delivery zone
+                this.deliveryZones.push(currentItem);
             }
         }
     }
 
-    preProcess(){
-        // Pre process the map to add nearest deliver zone and nearest spawn point
-        // Add delivery cells to queue
-        let deliverQueue = new Queue()
-        this.delivery_cells.forEach((item) =>{
-            deliverQueue.enqueue(item)
-        })
-
-        // while we have some tiles to explore
-        while(!deliverQueue.isEmpty()){
-            let currentTile = deliverQueue.dequeue()
-        }
-
+    getItem(x, y){
+        return this.map[x][y];
     }
 }
 
