3ef165c503fcc8626058b797f9fccd38cd04eafd
TLZ2000 <thomas@cnt3.com>
2025-04-30 16:49:45 +0200

    Dumb Agent Working

--- prova/grafoMatrix.js
+++ prova/grafoMatrix.js
@@ -6,7 +6,7 @@ const client = new DeliverooApi(
     //'https://deliveroojs2.rtibdi.disi.unitn.it/',
     // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQyNmQ1NyIsIm5hbWUiOiJtYXJjbyIsInRlYW1JZCI6ImM3ZjgwMCIsInRlYW1OYW1lIjoiZGlzaSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQwMDA3NjIwfQ.1lfKRxSSwj3_a4fWnAV44U1koLrphwLkZ9yZnYQDoSw'
     'http://localhost:8080', 
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjdmMzMxMCIsIm5hbWUiOiJwcm92YSIsInRlYW1JZCI6IjYyZmQwNCIsInRlYW1OYW1lIjoidGVhbSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQ1ODIyNDAwfQ.KR5jRXJIuVo5LnM0JRJjTihfBymefwLMapNxmnR1Jb8"
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNkNjU1NCIsIm5hbWUiOiJBZ2VudCIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQ2MDE4MzU1fQ.Qj5XIXCpnxv4ibSukP8xL0oTz6X6v7_3ouKZiVBHJl8"
 )
 
 const me = {id: null, name: null, x: null, y: null, score: null};
@@ -18,6 +18,12 @@ client.onYou( ( {id, name, x, y, score} ) => {
     me.y = y
     me.score = score
 } )
+
+function distance( {x:x1, y:y1}, {x:x2, y:y2}) {
+    const dx = Math.abs( Math.round(x1) - Math.round(x2) )
+    const dy = Math.abs( Math.round(y1) - Math.round(y2) )
+    return dx + dy;
+}
 
 class Queue {
     constructor() {
@@ -322,14 +328,24 @@ client.onParcelsSensing( async ( pp ) => {
     }
 });
 
-function navigateBFS (initialPos, finalPos) {
+client.onParcelsSensing( optionsGeneration )
+client.onAgentsSensing( optionsGeneration )
+client.onYou( optionsGeneration )
 
+function navigateBFS (initialPos, finalPos) {
+    
     let queue = new Queue();
     let explored = new Set();
     let finalPath = undefined;
 
+    let initialNode = grafo.graphMap[initialPos[0]][initialPos[1]];
+
+    if(initialNode==undefined){
+        return undefined;
+    }
+
     // Add initial node to the queue
-    queue.enqueue({currentNode: grafo.graphMap[initialPos[0]][initialPos[1]], path: []});
+    queue.enqueue({currentNode: initialNode, path: []});
 
     // Cycle until the queue is empty or a valid path has been found
     while (!queue.isEmpty()) {
@@ -357,7 +373,7 @@ function navigateBFS (initialPos, finalPos) {
                 let tmp = path.slice();
                 tmp.push("U");
                 queue.enqueue({currentNode: currentNode.neighU, path: tmp});
-                console.log("U");
+                //console.log("U");
             }
 
             // Right
@@ -365,7 +381,7 @@ function navigateBFS (initialPos, finalPos) {
                 let tmp = path.slice();
                 tmp.push("R");
                 queue.enqueue({currentNode: currentNode.neighR, path: tmp});
-                console.log("R");
+                //console.log("R");
             }
 
             // Down
@@ -373,7 +389,7 @@ function navigateBFS (initialPos, finalPos) {
                 let tmp = path.slice();
                 tmp.push("D");
                 queue.enqueue({currentNode: currentNode.neighD, path: tmp});
-                console.log("D");
+                //console.log("D");
             }
 
             // Left
@@ -381,9 +397,9 @@ function navigateBFS (initialPos, finalPos) {
                 let tmp = path.slice();
                 tmp.push("L");
                 queue.enqueue({currentNode: currentNode.neighL, path: tmp});
-                console.log("L");
+                //console.log("L");
             }
-            console.log("------------------");
+            //console.log("------------------");
         }
     }
 
@@ -396,4 +412,370 @@ function navigateBFS (initialPos, finalPos) {
     }
 }
 
-console.log(navigateBFS([2,2], [5,5]));
+function optionsGeneration () {
+
+    // TODO revisit beliefset revision so to trigger option generation only in the case a new parcel is observed
+
+    /**
+     * Options generation
+     */
+    const options = []
+    for (const parcel of parcels.values()){
+        if ( !parcel.carriedBy ){
+            options.push( [ 'go_pick_up', parcel.x, parcel.y, parcel.id ] );
+            // myAgent.push( [ 'go_pick_up', parcel.x, parcel.y, parcel.id ] )
+        } else if (parcel.carriedBy == me.id){
+            options.push( [ 'go_deliver' ] );
+        }
+    }
+        
+            
+    /**
+     * Options filtering
+     */
+    let best_option;
+    let nearest = Number.MAX_VALUE;
+    for (const option of options) {
+        if ( option[0] == 'go_pick_up' ) {
+            let [go_pick_up,x,y,id] = option;
+            let current_d = navigateBFS([Math.round(me.x), Math.round(me.y)], [x,y]);
+            if(current_d!=undefined){
+                current_d=current_d.length;
+                if ( current_d < nearest ) {
+                    best_option = option
+                    nearest = current_d
+                }
+            }            
+        } else if (option[0] == "go_deliver"){
+            best_option = option;
+            break;
+        }
+    }
+
+    /**
+     * Best option is selected
+     */
+    if ( best_option )
+        //console.log(best_option[1] + " "+ best_option[2])
+        myAgent.push( best_option )
+
+}
+
+/**
+ * Intention revision loop
+ */
+class IntentionRevision {
+
+    #intention_queue = new Array();
+    get intention_queue () {
+        return this.#intention_queue;
+    }
+
+    async loop ( ) {
+        while ( true ) {
+            // Consumes intention_queue if not empty
+            if ( this.intention_queue.length > 0 ) {
+                console.log( 'intentionRevision.loop', this.intention_queue.map(i=>i.predicate) );
+            
+                // Current intention
+                const intention = this.intention_queue[0];
+                
+                // Is queued intention still valid? Do I still want to achieve it?
+                // TODO this hard-coded implementation is an example
+                let id = intention.predicate[2]
+                let p = parcels.get(id)
+                if ( p && p.carriedBy ) {
+                    console.log( 'Skipping intention because no more valid', intention.predicate )
+                    continue;
+                }
+
+                // Start achieving intention
+                await intention.achieve()
+                // Catch eventual error and continue
+                .catch( error => {
+                    // console.log( 'Failed intention', ...intention.predicate, 'with error:', ...error )
+                } );
+
+                // Remove from the queue
+                this.intention_queue.shift();
+            }
+            // Postpone next iteration at setImmediate
+            await new Promise( res => setImmediate( res ) );
+        }
+    }
+
+    // async push ( predicate ) { }
+
+    log ( ...args ) {
+        console.log( ...args )
+    }
+
+}
+
+class IntentionRevisionReplace extends IntentionRevision {
+
+    async push ( predicate ) {
+
+        // Check if already queued
+        const last = this.intention_queue.at( this.intention_queue.length - 1 );
+        if ( last && last.predicate.join(' ') == predicate.join(' ') ) {
+            return; // intention is already being achieved
+        }
+        
+        console.log( 'IntentionRevisionReplace.push', predicate );
+        const intention = new Intention( this, predicate );
+        this.intention_queue.push( intention );
+        
+        // Force current intention stop 
+        if ( last ) {
+            last.stop();
+        }
+    }
+
+}
+
+const myAgent = new IntentionRevisionReplace();
+myAgent.loop();
+
+
+/**
+ * Intention
+ */
+class Intention {
+
+    // Plan currently used for achieving the intention 
+    #current_plan;
+    
+    // This is used to stop the intention
+    #stopped = false;
+    get stopped () {
+        return this.#stopped;
+    }
+    stop () {
+        // this.log( 'stop intention', ...this.#predicate );
+        this.#stopped = true;
+        if ( this.#current_plan)
+            this.#current_plan.stop();
+    }
+
+    /**
+     * #parent refers to caller
+     */
+    #parent;
+
+    /**
+     * @type { any[] } predicate is in the form ['go_to', x, y]
+     */
+    get predicate () {
+        return this.#predicate;
+    }
+    /**
+     * @type { any[] } predicate is in the form ['go_to', x, y]
+     */
+    #predicate;
+
+    constructor ( parent, predicate ) {
+        this.#parent = parent;
+        this.#predicate = predicate;
+    }
+
+    log ( ...args ) {
+        if ( this.#parent && this.#parent.log )
+            this.#parent.log( '\t', ...args )
+        else
+            console.log( ...args )
+    }
+
+    #started = false;
+    /**
+     * Using the plan library to achieve an intention
+     */
+    async achieve () {
+        // Cannot start twice
+        if ( this.#started)
+            return this;
+        else
+            this.#started = true;
+
+        // Trying all plans in the library
+        for (const planClass of planLibrary) {
+
+            // if stopped then quit
+            if ( this.stopped ) throw [ 'stopped intention', ...this.predicate ];
+
+            // if plan is 'statically' applicable
+            if ( planClass.isApplicableTo( ...this.predicate ) ) {
+                // plan is instantiated
+                this.#current_plan = new planClass(this.#parent);
+                this.log('achieving intention', ...this.predicate, 'with plan', planClass.name);
+                // and plan is executed and result returned
+                try {
+                    const plan_res = await this.#current_plan.execute( ...this.predicate );
+                    this.log( 'successful intention', ...this.predicate, 'with plan', planClass.name, 'with result:', plan_res );
+                    return plan_res
+                // or errors are caught so to continue with next plan
+                } catch (error) {
+                    this.log( 'failed intention', ...this.predicate,'with plan', planClass.name, 'with error:', error );
+                }
+            }
+
+        }
+
+        // if stopped then quit
+        if ( this.stopped ) throw [ 'stopped intention', ...this.predicate ];
+
+        // no plans have been found to satisfy the intention
+        // this.log( 'no plan satisfied the intention ', ...this.predicate );
+        throw ['no plan satisfied the intention ', ...this.predicate ]
+    }
+}
+
+/**
+ * Plan library
+ */
+const planLibrary = [];
+
+class Plan {
+
+    // This is used to stop the plan
+    #stopped = false;
+    stop () {
+        // this.log( 'stop plan' );
+        this.#stopped = true;
+        for ( const i of this.#sub_intentions ) {
+            i.stop();
+        }
+    }
+    get stopped () {
+        return this.#stopped;
+    }
+
+    /**
+     * #parent refers to caller
+     */
+    #parent;
+
+    constructor ( parent ) {
+        this.#parent = parent;
+    }
+
+    log ( ...args ) {
+        if ( this.#parent && this.#parent.log )
+            this.#parent.log( '\t', ...args )
+        else
+            console.log( ...args )
+    }
+
+    // this is an array of sub intention. Multiple ones could eventually being achieved in parallel.
+    #sub_intentions = [];
+
+    async subIntention ( predicate ) {
+        const sub_intention = new Intention( this, predicate );
+        this.#sub_intentions.push( sub_intention );
+        return sub_intention.achieve();
+    }
+
+}
+
+class GoPickUp extends Plan {
+
+    static isApplicableTo ( go_pick_up, x, y, id ) {
+        return go_pick_up == 'go_pick_up';
+    }
+
+    async execute ( go_pick_up, x, y ) {
+        if ( this.stopped ) throw ['stopped']; // if stopped then quit
+        await this.subIntention( ['go_to', x, y] );
+        if ( this.stopped ) throw ['stopped']; // if stopped then quit
+        await client.emitPickup()
+        if ( this.stopped ) throw ['stopped']; // if stopped then quit
+        return true;
+    }
+
+}
+
+class GoDeliver extends Plan {
+
+    static isApplicableTo (go_deliver) {
+        return go_deliver == 'go_deliver';
+    }
+
+    async execute (go_deliver) {
+        if ( this.stopped ) throw ['stopped']; // if stopped then quit
+
+        let nearestDelivery = (grafo.graphMap[Math.round(me.x)][Math.round(me.y)]).visitedDeliveries[0].deliveryNode;
+        
+        await this.subIntention( ['go_to', nearestDelivery.x, nearestDelivery.y] );
+        if ( this.stopped ) throw ['stopped']; // if stopped then quit
+        await client.emitPutdown();
+        if ( this.stopped ) throw ['stopped']; // if stopped then quit
+        return true;
+    }
+
+}
+
+class BlindBFSmove extends Plan {
+
+    static isApplicableTo ( go_to, x, y ) {
+        return go_to == 'go_to';
+    }
+
+    async execute ( go_to, x, y ) {
+        let path = navigateBFS([Math.round(me.x), Math.round(me.y)], [x,y]);
+
+        let i = 0;
+        while ( i < path.length ) {
+
+            if ( this.stopped ) throw ['stopped']; // if stopped then quit
+
+            let moved_horizontally;
+            let moved_vertically;
+            
+            // this.log('me', me, 'xy', x, y);
+
+            if ( path[i]=="R" )
+                moved_horizontally = await client.emitMove('right')
+                // status_x = await this.subIntention( 'go_to', {x: me.x+1, y: me.y} );
+            else if ( path[i]=="L" )
+                moved_horizontally = await client.emitMove('left')
+                // status_x = await this.subIntention( 'go_to', {x: me.x-1, y: me.y} );
+
+            if (moved_horizontally) {
+                me.x = moved_horizontally.x;
+                me.y = moved_horizontally.y;
+            }
+
+            if ( this.stopped ) throw ['stopped']; // if stopped then quit
+
+            if ( path[i]=="U" )
+                moved_vertically = await client.emitMove('up')
+                // status_x = await this.subIntention( 'go_to', {x: me.x, y: me.y+1} );
+            else if ( path[i]=="D" )
+                moved_vertically = await client.emitMove('down')
+                // status_x = await this.subIntention( 'go_to', {x: me.x, y: me.y-1} );
+
+            if (moved_vertically) {
+                me.x = moved_vertically.x;
+                me.y = moved_vertically.y;
+            }
+            
+            if ( ! moved_horizontally && ! moved_vertically) {
+                this.log('stucked');
+                throw 'stucked';
+            } else if ( me.x == x && me.y == y ) {
+                // this.log('target reached');
+            }
+
+            i++;
+            
+        }
+
+        return true;
+
+    }
+}
+
+// plan classes are added to plan library 
+planLibrary.push( GoPickUp )
+planLibrary.push( BlindBFSmove )
+planLibrary.push( GoDeliver )
